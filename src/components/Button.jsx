1069a8a3f720228b514ef75793c2af4aaf02c59a
Agraj Yadav <agrajy10@gmail.com>
2022-01-30 19:06:46 +0530

    Buttton and TextField component

---
+++ src/components/Button.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const ButtonEl = styled.button`
+  display: inline-block;
+  padding: 0.8125rem;
+  min-width: 158px;
+  background-color: ${({ variant, theme }) => theme.button[variant].bg};
+  color: ${({ variant, theme }) => theme.button[variant].color};
+  font-weight: 700;
+  font-size: 0.875rem;
+  line-height: 1;
+  border: none;
+  border-radius: 0.625rem;
+  cursor: pointer;
+  user-select: none;
+  &:hover {
+    background-color: ${({ variant, theme }) => theme.button[variant].hover.bg};
+    color: ${({ variant, theme }) => theme.button[variant].hover.color};
+  }
+`;
+
+function Button({ variant = 'primary', children }) {
+  return <ButtonEl variant={variant}>{children}</ButtonEl>;
+}
+
+export default Button;
---
+++ src/components/TextField.jsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+const Label = styled.label`
+  display: block;
+  font-size: 0.875rem;
+  line-height: 1;
+  font-weight: 700;
+  margin-bottom: 5px;
+  letter-spacing: -0.19px;
+`;
+
+const LabelDesc = styled.p`
+  color: ${({ theme }) => theme.text.color1};
+  font-size: 0.875rem;
+  margin: 0 0 1rem 0;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 3rem;
+  padding: 0.8125rem 1.5rem;
+  font-size: 0.9375rem;
+  border-radius: 0.3125rem;
+  background-color: ${({ theme }) => theme.textField.bg};
+  color: ${({ theme }) => theme.textField.color};
+  border: 1px solid ${({ theme }) => theme.textField.borderColor};
+  outline: 0;
+  &:focus {
+    border-color: ${({ theme }) => theme.textField.active.borderColor};
+  }
+`;
+
+function TextField() {
+  return (
+    <>
+      <Label>Feedback Title</Label>
+      <LabelDesc>Add a short, descriptive headline</LabelDesc>
+      <Input type="text" />
+    </>
+  );
+}
+
+export default TextField;
