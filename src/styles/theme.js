80d3b4f2448ffcf28d3b20f113ac84ef68abcec0
Agraj Yadav <agrajy10@gmail.com>
2022-01-30 18:56:06 +0530

    nesting correction

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -37,66 +37,66 @@ const theme = {
         bg: '#E98888',
         color: '#F2F4FE'
       }
-    },
-    upVoteButton: {
-      bg: '#F2F4FE',
+    }
+  },
+  upVoteButton: {
+    bg: '#F2F4FE',
+    color: '#3A4374',
+    arrow: '#4661E6',
+    hover: {
+      bg: '#CFD7FF',
       color: '#3A4374',
-      arrow: '#4661E6',
-      hover: {
-        bg: '#CFD7FF',
-        color: '#3A4374',
-        arrow: '#4661E6'
-      },
-      active: {
-        bg: '#4661E6',
-        color: '#FFFFFF',
-        arrow: '#FFFFFF'
-      }
+      arrow: '#4661E6'
     },
-    category: {
-      bg: '#F2F4FF',
-      color: '#4661E6',
-      hover: {
-        bg: '#CFD7FF',
-        color: '#4661E6'
-      },
-      active: {
-        bg: '#4661E6',
-        color: '#FFFFFF'
-      }
+    active: {
+      bg: '#4661E6',
+      color: '#FFFFFF',
+      arrow: '#FFFFFF'
+    }
+  },
+  category: {
+    bg: '#F2F4FF',
+    color: '#4661E6',
+    hover: {
+      bg: '#CFD7FF',
+      color: '#4661E6'
     },
-    label: {
-      color: '#3A4374',
-      descColor: '#647196'
+    active: {
+      bg: '#4661E6',
+      color: '#FFFFFF'
+    }
+  },
+  label: {
+    color: '#3A4374',
+    descColor: '#647196'
+  },
+  textField: {
+    bg: '#F7F8FD',
+    color: '#3A4374',
+    borderColor: '#F7F8FD',
+    active: {
+      borderColor: '#4661E6'
     },
-    textField: {
-      bg: '#F7F8FD',
-      color: '#3A4374',
-      borderColor: '#F7F8FD',
-      active: {
-        borderColor: '#4661E6'
-      },
-      error: {
-        color: '#D73737',
-        borderColor: '#D73737'
-      }
-    },
-    roadmap: {
-      bg: '#FFFFFF',
-      planned: '#F49F85',
-      inProgress: '#AD1FEA',
-      live: '#62BCFA'
-    },
-    feedbackItem: {
-      bg: '#FFFFFF',
-      commentIcon: '#CDD2EE',
-      commentIconText: '#3A4374'
-    },
-    commentList: {
-      bg: '#FFFFFF',
-      replyButton: {
-        color: '#4661E6'
-      }
+    error: {
+      color: '#D73737',
+      borderColor: '#D73737'
+    }
+  },
+  roadmap: {
+    bg: '#FFFFFF',
+    planned: '#F49F85',
+    inProgress: '#AD1FEA',
+    live: '#62BCFA'
+  },
+  feedbackItem: {
+    bg: '#FFFFFF',
+    commentIcon: '#CDD2EE',
+    commentIconText: '#3A4374'
+  },
+  commentList: {
+    bg: '#FFFFFF',
+    replyButton: {
+      color: '#4661E6'
     }
   }
 };
