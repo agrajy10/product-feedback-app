927ac6fa813fe24bba1baf2a8f668302a643ea93
Agraj Yadav <agrajy10@gmail.com>
2022-01-31 10:45:59 +0530

    Homepage sortby component completed

--- src/components/SortBy.jsx
+++ src/components/SortBy.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 2px;
+`;
+
+const Label = styled.label`
+  flex: 0 0 auto;
+  font-size: 13px;
+  color: #f2f4fe;
+  white-space: nowrap;
+`;
+
+const Select = styled.select`
+  flex: 1 1 0%;
+  font-size: 14px;
+  font-weight: 700;
+  color: #f2f4fe;
+  background: transparent;
+  border: none;
+`;
 
 function SortBy() {
-  return <div>Sort by</div>;
+  return (
+    <Wrapper>
+      <Label htmlFor="sortby">Sort By :</Label>
+      <Select id="sortby">
+        <option value="">Most upvotes</option>
+        <option value="">Least upvotes</option>
+        <option value="">Most comments</option>
+        <option value="">Least comments</option>
+      </Select>
+    </Wrapper>
+  );
 }
 
 export default SortBy;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 import Container from '../layout/Container';
 import Header from '../components/Header';
 import MobileHeader from '../components/MobileHeader';
@@ -9,6 +11,15 @@ import useWindowSize from '../hooks/useWindowSize';
 
 import breakpoints from '../styles/breakpoints';
 
+const SortByWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #373f68;
+  margin: 0 -24px;
+  padding: 8px 24px;
+`;
+
 function Home() {
   const { width } = useWindowSize();
 
@@ -17,9 +28,11 @@ function Home() {
   return (
     <Container>
       {pageHeader}
-      <SortBy />
+      <SortByWrapper>
+        <SortBy />
+        <Button>+ Add Feedback</Button>
+      </SortByWrapper>
       <FeedbackList />
-      <Button>+ Add Feedback</Button>
     </Container>
   );
 }
