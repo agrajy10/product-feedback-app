8546f1bdd59ea786ced4c010878b54e64e375819
Agraj Yadav <agrajy10@gmail.com>
2022-02-02 21:22:21 +0530

    Forgot password functionality completed

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,20 +1,25 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import { sendPasswordResetEmail } from 'firebase/auth';
 
 import Container from '../layout/Container';
 import BackButton from '../components/BackButton';
 import Button from '../components/Button';
 import FormCard from '../components/FormCard';
 import TextField from '../components/TextField';
+import Alert from '../components/Alert';
 
 import breakpoints from '../styles/breakpoints';
+
+import { auth } from '../firebase-config';
 
 import IconNewFeedback from '../assets/shared/icon-new-feedback.svg';
 
 const FormContainer = styled(Container)`
   max-width: 540px;
-  input,
-  select,
-  textarea {
+  .field-wrap {
     margin-bottom: 24px;
   }
 `;
@@ -42,21 +47,56 @@ const FormBottom = styled.div`
   }
 `;
 
+const initialValues = {
+  email: ''
+};
+
+const validationSchema = Yup.object({
+  email: Yup.string().email('Invalid email').required("Can't be empty")
+});
+
 function FrogotPassword() {
+  const [message, setMessage] = useState({ type: '', content: '' });
+
+  const onSubmit = async ({ email }) => {
+    setMessage({ type: '', content: '' });
+    try {
+      await sendPasswordResetEmail(auth, email);
+      setMessage({
+        type: 'success',
+        content: 'Reset link sent successfully.'
+      });
+    } catch (error) {
+      setMessage({ type: 'error', content: error.message });
+    }
+  };
+
   return (
     <FormContainer>
       <main>
         <BackButton>Go Back</BackButton>
         <FormCard icon={IconNewFeedback}>
           <FormHeading>Reset password</FormHeading>
-          <form>
-            <TextField id="email" label="Email" />
-            <FormBottom>
-              <Button type="submit" className="submit-btn">
-                Send reset link
-              </Button>
-            </FormBottom>
-          </form>
+          {message.content && <Alert variant={message.type}>{message.content}</Alert>}
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}>
+            {({ isSubmitting }) => {
+              return (
+                <Form>
+                  <div className="field-wrap">
+                    <TextField id="email" name="email" label="Email" />
+                  </div>
+                  <FormBottom>
+                    <Button type="submit" className="submit-btn" disabled={isSubmitting}>
+                      Send reset link
+                    </Button>
+                  </FormBottom>
+                </Form>
+              );
+            }}
+          </Formik>
         </FormCard>
       </main>
     </FormContainer>
