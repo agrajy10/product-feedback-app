fbc97017b3abdb4724fabdf59763ff3039d44bce
Agraj Yadav <agrajy10@gmail.com>
2022-02-03 08:46:10 +0530

    updated button component to use as Link or Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const ButtonEl = styled.button`
   display: inline-block;
@@ -23,9 +24,36 @@ const ButtonEl = styled.button`
   }
 `;
 
-function Button({ className, variant = 'primary', type = 'button', children, ...props }) {
-  return (
-    <ButtonEl className={className} type={type} variant={variant} {...props}>
+const LinkEl = styled(Link)`
+  display: inline-block;
+  padding: 0.8125rem;
+  min-width: 158px;
+  background-color: ${({ variant, theme }) => theme.button[variant].bg};
+  color: ${({ variant, theme }) => theme.button[variant].color};
+  font-weight: 700;
+  font-size: 0.875rem;
+  line-height: 1;
+  border: none;
+  border-radius: 0.625rem;
+  cursor: pointer;
+  user-select: none;
+  &:hover {
+    background-color: ${({ variant, theme }) => theme.button[variant].hover.bg};
+    color: ${({ variant, theme }) => theme.button[variant].hover.color};
+  }
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+`;
+
+function Button({ href, className, variant = 'primary', type = 'button', children, ...props }) {
+  return href ? (
+    <LinkEl to={href} className={className} variant={variant} {...props}>
+      {children}
+    </LinkEl>
+  ) : (
+    <ButtonEl type={type} className={className} variant={variant} {...props}>
       {children}
     </ButtonEl>
   );
